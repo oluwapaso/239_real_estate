c87e450d696098a403379784ead0ee8fbf960687
oluwapaso <adebayoishola01@gmail.com>
2024-07-01 16:20:16 +0100

    Subtract 4 hours from ast alert date when seecting properties

--- _repo/listings_repo.ts
+++ _repo/listings_repo.ts
@@ -170,7 +170,7 @@ export class MysqlListingsRepo implements ListingsRepo {
 
                 let last_alert = params.last_alert;
                 last_alert = moment(last_alert);
-                const lastAlert = last_alert.subtract(4, 'hours').format('YYYY-MM-DD HH:mm:ss');
+                const lastAlert = last_alert.subtract(5, 'hours').format('YYYY-MM-DD HH:mm:ss');
 
                 [rows] = await connection.query<RowDataPacket[]>(`SELECT ${fields}, (SELECT COUNT(*) AS total_records FROM properties WHERE 
                 Status='Active' AND MatrixModifiedDT > '${lastAlert}' ${search_filter}) AS total_records FROM properties WHERE 
