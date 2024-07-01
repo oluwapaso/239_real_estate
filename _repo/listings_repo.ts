854026d42e2f780e7e33b4761946a14dec5bdfbc
oluwapaso <adebayoishola01@gmail.com>
2024-07-01 17:14:25 +0100

    Subtract 5 hours from ast alert date when seecting properties

--- _repo/listings_repo.ts
+++ _repo/listings_repo.ts
@@ -172,10 +172,7 @@ export class MysqlListingsRepo implements ListingsRepo {
                 last_alert = moment(last_alert);
                 const lastAlert = last_alert.subtract(5, 'hours').format('YYYY-MM-DD HH:mm:ss');
 
-                console.log(`SELECT ${fields}, (SELECT COUNT(*) AS total_records FROM properties WHERE 
-                Status='Active' AND MatrixModifiedDT >= '${lastAlert}' ${search_filter}) AS total_records FROM properties WHERE 
-                Status='Active' AND MatrixModifiedDT >= '${lastAlert}' ${search_filter} 
-                ORDER BY ${order_by} LIMIT ${start_from}, ${limit}`);
+                console.log(`SELECT ${fields}, (SELECT COUNT(*) AS total_records FROM properties WHERE Status='Active' AND MatrixModifiedDT >= '${lastAlert}' ${search_filter}) AS total_records FROM properties WHERE Status='Active' AND MatrixModifiedDT >= '${lastAlert}' ${search_filter} ORDER BY ${order_by} LIMIT ${start_from}, ${limit}`);
 
                 [rows] = await connection.query<RowDataPacket[]>(`SELECT ${fields}, (SELECT COUNT(*) AS total_records FROM properties WHERE 
                 Status='Active' AND MatrixModifiedDT >= '${lastAlert}' ${search_filter}) AS total_records FROM properties WHERE 
