a0ce2562f9cee9a13722026dfdab48620183bc54
oluwapaso <adebayoishola01@gmail.com>
2024-09-21 21:20:51 +0100

    Fixing automation

--- app/(admin)/admin/(dashboard)/(automations)/edit-automation/page.tsx
+++ app/(admin)/admin/(dashboard)/(automations)/edit-automation/page.tsx
@@ -113,9 +113,12 @@ const EditAutomation = () => {
                 BuildTriggers(detailsResp.trigger);
                 setIsPublished(detailsResp.published_version);
 
+                console.log("detailsResp.automation_status", detailsResp.automation_status)
                 if (detailsResp.automation_status == "Active") {
+                    console.log("Is Active")
                     const edit_btn = document.querySelector(".edit_btn") as HTMLElement;
                     if (edit_btn) {
+                        console.log("edit_btn found")
                         edit_btn.style.display = "block";
                     }
 
@@ -129,8 +132,10 @@ const EditAutomation = () => {
                         publish_btn.style.display = "none";
                     }
                 } else {
+                    console.log("Is Not active")
                     const edit_btn = document.querySelector(".edit_btn") as HTMLElement;
                     if (edit_btn) {
+                        console.log("edit_btn found")
                         edit_btn.style.display = "none";
                     }
 
@@ -812,6 +817,7 @@ const EditAutomation = () => {
                 }
 
                 setTriger(newTrigger.trigger);
+                setTrigerEvent(newTrigger.trigger);
                 setAutomationInfoSteps((prev_val) => {
                     return {
                         ...prev_val,
