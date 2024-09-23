d84db339428550049824a3af323d00a2954bd665
oluwapaso <adebayoishola01@gmail.com>
2024-09-23 09:17:23 +0100

    fixing front end

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -277,7 +277,8 @@ function NavBar({ page }: NavProps) {
                 </div>
             </div>
 
-            <div className={`w-full ${menuOpen ? "block backdrop-blur bg-black bg-opacity-35" : "hidden"} absolute z-[150] h-[100vh] bottom-0 right-0 top-0 left-0`}>
+            <div className={`w-full ${menuOpen ? "block backdrop-blur bg-black bg-opacity-35" : "hidden"} 
+                absolute z-[150] h-[100dvh] bottom-0 right-0 top-0 left-0`}>
                 <div id='mobile_menu' className='bg-primary h-[100vh] flex absolute right-0 top-0 w-[100%] max-w-[350px]'>
                     <div className='w-full h-full *:text-white *:font-play-fair-display relative'>
                         <div className='flex justify-between items-center p-4 border-b border-white'>
